e6b98655c3434c6792c84a89908b96e2617843ca
burkaans <raimonds.buholts@gmail.com>
2018-05-23 20:18:39 +0300

    initial commit
    Java_day_2

---
+++ src/ConditionalMath.java
@@ -0,0 +1,17 @@
+
+public class ConditionalMath {
+
+	public static void main(String[] args) {
+		
+		int someNumber = 10;
+		
+		if(someNumber ==10) {
+			System.out.println("ir 10");
+		} else {
+			System.out.println("nav 10");
+		}
+		System.out.println("END!");
+
+	}
+
+}
